8a978c741dfdb6264b990ceac6d57758202c2a7b
Feliciano Romero <felicianoromero.sec@institutolamerced.edu.ar>
2024-03-14 10:20:13 -0300

    Server - A method added to /

--- Server/routes.py
+++ Server/routes.py
@@ -4,4 +4,9 @@ from flask import request, make_response, jsonify
 class Home(MethodView):
     def get(self):
         return 'Hello World!'
-    
+    def post(self):
+        data = request.get_json()
+        if data["msg"]:
+            return data["msg"]
+        else:
+            return None
